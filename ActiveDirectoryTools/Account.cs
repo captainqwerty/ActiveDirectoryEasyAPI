5fd3721c3df5a6d6dc4267d8213ad85324db9d81
captainqwerty <antonybragg@gmail.com>
2018-07-21 20:01:46 +0100

    Basic implementation of PasswordReset

---
+++ ActiveDirectoryTools/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices.AccountManagement;
+
+namespace ActiveDirectoryTools
+{
+    public class Account
+    {
+        /// <summary>
+        /// Reset an account password.
+        /// </summary>
+        /// <param name="username">Enter the username.</param>
+        /// <param name="password">Enter the users new password.</param>
+        /// <param name="expireNow">User reset password at next logon.</param>
+        public void PasswordReset(string username, string password, bool expireNow = true)
+        {
+            using (var principalContext = new PrincipalContext(ContextType.Domain))
+            {
+                using (var user = UserPrincipal.FindByIdentity(principalContext, username))
+                {
+                    if (user != null)
+                    {
+                        user.SetPassword(password);
+
+                        if(expireNow)
+                        {
+                            user.ExpirePasswordNow();
+                        }
+                    }
+                    else
+                    {
+                        // Throw error
+                    }
+                }
+            }
+        }
+    }
+}
