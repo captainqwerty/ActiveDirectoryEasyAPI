0124e017495a0ca0ed02a79a82e3c42e8f334b23
CaptainQwerty <antonybragg@gmail.com>
2018-09-13 16:23:33 +0100

    Added Audit Class.

---
+++ ActiveDirectoryTools/Audit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveDirectoryTools
+{
+    public class Audit
+    {
+        public List<UserAccount> GetAllLockedOutAccounts(string organisationalUnit)
+        {
+            var accountTools = new Account();
+
+            var lockedUsers = new List<UserAccount>();
+
+            using (var context = new PrincipalContext(ContextType.Domain, "GEN2TRAINING", organisationalUnit))
+            {
+                var qbeUser = new UserPrincipal(context);
+
+                // create your principal searcher passing in the QBE principal    
+                var srch = new PrincipalSearcher(qbeUser);
+
+                // find all matches
+                foreach (var userPrincipal in srch.FindAll())
+                {
+                    var user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, userPrincipal.SamAccountName);
+
+                    if (!user.IsAccountLockedOut()) continue;
+
+                    lockedUsers.Add(accountTools.GetUserAccountDetails(user.UserPrincipalName));
+                    return lockedUsers;
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -10,7 +10,16 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
-            
+            var auditTool = new Audit();
+
+            var lockedOutAccounts = auditTool.GetAllLockedOutAccounts("OU=Lillyhall,OU=Students,OU=User Accounts,DC=gen2training,DC=co,DC=uk");
+
+            foreach (var user in lockedOutAccounts)
+            {
+                Console.WriteLine(user.Username);
+            }
+
+            Console.ReadLine();
         }
     }
 }
