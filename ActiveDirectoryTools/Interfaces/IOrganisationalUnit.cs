adcb35ef9412b4767e0f62de90818d6430cbd1c0
CaptainQwerty <antonybragg@gmail.com>
2018-11-02 14:53:44 +0000

    Update.

--- ActiveDirectoryTools/Interfaces/IOrganisationalUnit.cs
+++ ActiveDirectoryTools/Interfaces/IOrganisationalUnit.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ActiveDirectoryTools.Interfaces
+﻿namespace ActiveDirectoryTools.Interfaces
 {
     public interface IOrganisationalUnit
     {
