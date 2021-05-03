96caacd5e1357c5f9cdac5f47aea3137116fc3fd
Antony Bragg <antonybragg@gmail.com>
2021-05-03 18:18:50 +0100

    OU Search for users
    Started adding OU search

--- ActiveDirectoryTools/AuditTasks.cs
+++ ActiveDirectoryTools/AuditTasks.cs
@@ -45,5 +45,29 @@ namespace ActiveDirectoryTools
             return users.Where(u1 => !users.Any(u2 => u2.UserPrincipalName == u1.UserPrincipalName && u2.LastLogon > u1.LastLogon))
                 .Where(u => (DateTime.Now - u.LastLogon) >= inactivityTime).ToList();
         }
+
+        public static List<UserAccount> GetUsersInOU(string organistionalUnit)
+        {
+            var users = new List<UserAccount>();
+
+            // create your domain context and define the OU container to search in
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain, "DOMAINNAME",
+                                                        "OU=SomeOU,dc=YourCompany,dc=com");
+
+            // define a "query-by-example" principal - here, we search for a UserPrincipal (user)
+            UserPrincipal qbeUser = new UserPrincipal(ctx);
+
+            // create your principal searcher passing in the QBE principal    
+            PrincipalSearcher srch = new PrincipalSearcher(qbeUser);
+
+            // find all matches
+            foreach (var found in srch.FindAll())
+            {
+                // do whatever here - "found" is of type "Principal" - it could be user, group, computer.....          
+                //users.Add(found.DisplayName);
+            }
+
+            return users;
+        }
     }
 }
